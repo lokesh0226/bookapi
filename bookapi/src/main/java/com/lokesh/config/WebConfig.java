837300230fd6b0e064a0730290914ace1b614cd5
lokesh0226 <lokeshbharvdwaj@gmail.com>
2019-09-28 08:18:26 -0700

    first commit
    uploading all files

---
+++ bookapi/src/main/java/com/lokesh/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.lokesh.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.bushansirgur.controller" })
+
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+}
